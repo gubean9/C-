cced4edc361dad8fcaec4d7975ab5d186090ab32
gubean9 <gubean9@gmail.com>
2023-11-13 15:44:24 +0900

    ㅇㅇ

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,15 @@
 ﻿namespace ConsoleApp2
 {
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class UserService
+    {
+        public void Login(User user) { }
+        public void Purchase(User user) { }
+    }
     internal class Program
     {
         static void Main(string[] args)
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StrategyPattern
+{
+    void class MallardDuck
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck mallard = new MallardDuck();
+            RubberDuck rubberDuckie = new RubberDuck();
+            DecoyDuck decoy = new DecoyDuck();
+
+            ModelDuck model = new ModelDuck();
+
+            mallard.PerformQuack();
+            rubberDuckie.PerformQuack();
+            decoy.PerformQuack();
+
+            model.PerformFly();
+            model.FlyBehavior = new FlyRocketPowered();
+            model.PerformFly();
+        }
+    }
+}

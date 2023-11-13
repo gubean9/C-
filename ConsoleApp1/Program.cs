4beb569d00f259b51d3ee43cfed6506911ba5283
gubean9 <gubean9@gmail.com>
2023-11-13 14:24:56 +0900

    초기커밋

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public int Age { get; set; }
+
+        public void Login()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public void Purchase()
+        {
+            Console.WriteLine(Age);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User TOM = new User();
+            TOM.Name = "톰";
+            TOM.Age = 20;
+
+            TOM.Login();
+            TOM.Purchase();
+        }
+    }
+}
